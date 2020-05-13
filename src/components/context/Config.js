c06ed617815a899ed0fc783c77f6291391e5bdf7
Denis-Florin Rendler <connect@rendler.me>
2020-05-13 13:15:05 +0300

    add env config

--- src/components/context/Config.js
+++ src/components/context/Config.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
+// here we store default values that do not change across envs
+// IMPORTANT: first level vars will be overridden and NOT merged
 export const config = {
-    web: {
-        storage_path: process.env.REACT_APP_SAFENOTE_STORAGE_PATH,
-        domain: process.env.REACT_APP_SAFENOTE_DOMAIN,
-    },
-    app: {
-        timeframe: [
-            {label: "1 hour", value: "1h"},
-            {label: "2 hour", value: "2h"},
-        ]
-    }
+
 };
 
 export const ConfigContext = React.createContext(config);
