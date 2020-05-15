943084dd66c9d3e5916c05096b6ceba8686b783f
Denis-Florin Rendler <connect@rendler.me>
2020-05-15 14:19:17 +0300

    d

--- webapp/react/src/components/context/Config.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// here we store default values that do not change across envs
-// IMPORTANT: first level vars will be overridden and NOT merged
-export const config = {
-
-};
-
-export const ConfigContext = React.createContext(config);
--- webapp/react/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders website link in header', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/by koderhut.eu/i);
-  expect(linkElement).toBeInTheDocument();
-});
